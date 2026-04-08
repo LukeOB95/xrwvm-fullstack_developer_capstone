e0dfba43344cf56f9bbdf99be0495faa453c3ac3
LukeOB95 <luke.oboyle@gmail.com>
2026-04-08 09:51:43 -0400

    22:51 PM, April 8th, 2026

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,20 +6,34 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+
+    # Paths
+
+    # registration
     path(route='register', view=views.registration, name='register'),
 
-    # path for login
+    # login
     path(route='login', view=views.login_user, name='login'),
 
-    # path for loggin out
+    # logging out
     path(route='logout', view=views.logout_user, name='logout'),
 
-    # path for getting all cars
+    # getting all cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
-    # path for dealer reviews view
-
-    # path for add a review view
+    # get all dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    
+    # get dealers by state
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    
+    # get dealers by id
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    
+    # get reviews by dealer id
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    
+    # adding a review
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
