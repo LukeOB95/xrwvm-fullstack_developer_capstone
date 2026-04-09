40735d9cf620cbac86e25e0f2e9a51a214dfb257
LukeOB95 <luke.oboyle@gmail.com>
2026-04-09 12:44:38 -0400

    01:44 AM April 10th, 2026

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,20 +19,23 @@ urlpatterns = [
     path(route='logout', view=views.logout_user, name='logout'),
 
     # getting all cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
     # get all dealers
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    
+
     # get dealers by state
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships, name='get_dealers_by_state'),
+
     # get dealers by id
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details, name='dealer_details'),
+
     # get reviews by dealer id
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    
+    path(route='reviews/dealer/<int:dealer_id>',
+         view=views.get_dealer_reviews, name='dealer_details'),
+
     # adding a review
     path(route='add_review', view=views.add_review, name='add_review'),
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -53,7 +53,7 @@ def logout_user(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
 
     # Load JSON data from the request body
     data = json.loads(request.body)
@@ -63,7 +63,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -138,7 +138,7 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
